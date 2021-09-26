7d00f44b10d6699cd3dbb8f73a3c6f1697fcfc82
daigokk <88434718+daigokk@users.noreply.github.com>
2021-09-26 18:37:23 +0900

    exsample

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include "wave.hpp"
+#include <math.h>
+#define PI acos(-1)
+int main(void)
+{
+    wave pcm(10);
+    for(int i = 0; i < pcm.size; i++)
+    {
+        double amp_left = sin(2 * PI * 0.25 / pcm.fs * i);
+        double amp_right = sin(2 * PI * 0.25 / pcm.fs * i + PI / 2);
+        double freq;
+        if(i < pcm.size * 1 / 10) freq = 261.626;
+        else if(i < pcm.size * 2 / 10) freq = 293.665;
+        else if(i < pcm.size * 3 / 10) freq = 329.628;
+        pcm.s[0][i] = amp_right * sin(2 * PI * freq / pcm.fs * i);
+        pcm.s[1][i] = amp_left *  sin(2 * PI * freq / pcm.fs * i);
+    }
+    pcm.write("test.wav");
+    return 0;
+}
